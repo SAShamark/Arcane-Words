bd604f9c524e028e711d46a9f8c596ab84d754bb
sashamark <sashamark7777@gmail.com>
2024-11-18 19:21:58 +0200

    Imported TextMeshPro and 2d Sprite

---
+++ Assets/Scripts/UI/Popups/PopupType.cs
@@ -0,0 +1,9 @@
+namespace UI.Screens
+{
+    public enum PopupType
+    {
+        None = 0,
+        MainMenu = 1,
+        Game = 2,
+    }
+}
---
+++ Assets/Scripts/UI/Screens/ScreenType.cs
@@ -0,0 +1,10 @@
+namespace UI.Screens
+{
+    public enum ScreenType
+    {
+        None = 0,
+        MainMenu = 1,
+        TwoPlayersGameMenu = 2,
+        SinglePlayerGameMenu = 3,
+    }
+}
