ca84e16ec6928b8d0402431c8d31976c7ae7b7af
sashamark <sashamark7777@gmail.com>
2024-11-25 11:49:29 +0200

    audio file separated

--- Assets/Scripts/Audio/Data/AudioConstants.cs
+++ Assets/Scripts/Audio/Data/AudioConstants.cs
@@ -5,10 +5,4 @@ namespace Audio.Data
         public const float MIN_VOLUME = -80f;
         public const float MAX_VOLUME = 0f;
     }
-    public struct SoundConstants
-    {
-        public const string BUTTON = "Button";
-        public const string UNPRESS_KEY = "UnpressKey";
-        public const string PRESS_KEY = "PressKey";
-    }
 }
---
+++ Assets/Scripts/Audio/Data/SoundConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Audio.Data
+{
+    public struct SoundConstants
+    {
+        public const string BUTTON = "Button";
+        public const string UNPRESS_KEY = "UnpressKey";
+        public const string PRESS_KEY = "PressKey";
+    }
+}
